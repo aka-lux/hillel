1522f433a878d72d60944e96d06932de6f36071c
@lux123 <alek3141@gmail.com>
2021-01-16 13:02:17 +0200

    Исправил замечания. Добавил в pom.xml maven-compiler-plugin и описание

--- src/main/java/com/zakharchenko/homeworks/hw_02/task_02.java
+++ src/main/java/com/zakharchenko/homeworks/hw_02/task_02.java
@@ -2,13 +2,15 @@ package com.zakharchenko.homeworks.hw_02;
 
 public class task_02 {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 4;
-        int c = 5;
-        double p = (a+b+c)/2;
+        double a = 3;
+        double b = 4;
+        double c = 5;
 
-        System.out.println("Input: a="+ a +", b="+ b +", c="+ c);
-        System.out.print("Result: " + Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+        double p = (a + b + c) / 2;
+        double d = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        System.out.println("Input: a=" + a + ", b=" + b + ", c=" + c);
+        System.out.print("Result: " + d);
 
     }
 }
--- src/main/java/com/zakharchenko/homeworks/hw_02/task_03.java
+++ src/main/java/com/zakharchenko/homeworks/hw_02/task_03.java
@@ -2,29 +2,26 @@ package com.zakharchenko.homeworks.hw_02;
 
 public class task_03 {
     public static void main(String[] args) {
-        int a = 1;
-        int b = -1;
-        int c = -2;
+        double a = 1;
+        double b = -1;
+        double c = -2;
 
-        int D = b*b-4*a*c;
-        int x1 = (int) (-b+Math.sqrt(D))/(2*a);
-        int x2 = (int) (-b-Math.sqrt(D))/(2*a);
+        double D = b * b - 4 * a * c;
+        double x1 = (-b + Math.sqrt(D)) / (2 * a);
+        double x2 = (-b - Math.sqrt(D)) / (2 * a);
+        double x = (-b / (2 * a));
 
-        System.out.println("Input: a="+ a +", b="+ b +", c="+ c);
+        System.out.println("Input: a=" + a + ", b=" + b + ", c=" + c);
 
-        if(D > 0){
-            if(x1 < x2){
-                System.out.println("Output: x1="+ x1 +" x2="+ x2);
-            }else{
-                System.out.println("Output: x1="+ x2 +" x2="+ x1);
+        if (D > 0) {
+            if (x1 < x2) {
+                System.out.println("Output: x1=" + x1 + " x2=" + x2);
+            } else {
+                System.out.println("Output: x1=" + x2 + " x2=" + x1);
             }
-        }
-
-        if(0 == D){
-            System.out.println("Output: x="+ (-b/(2*a)) );
-        }
-
-        if(D < 0){
+        } else if (0 == D) {
+            System.out.println("Output: x=" + x);
+        } else if (D < 0) {
             System.out.println("Output: no roots");
         }
 

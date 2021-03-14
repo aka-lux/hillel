0d015bd285fe177f4907cfbc4d91d9ba275524d7
@lux123 <alek3141@gmail.com>
2021-03-14 17:03:56 +0200

    homework 18

---
+++ src/main/java/com/zakharchenko/homeworks/homework_18/Company.java
@@ -0,0 +1,13 @@
+package com.zakharchenko.homeworks.homework_18;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Data
+@NoArgsConstructor
+public class Company {
+    String name;
+    String catchPhrase;
+    String bs;
+}
---
+++ src/main/java/com/zakharchenko/homeworks/homework_18/Geo.java
@@ -0,0 +1,12 @@
+package com.zakharchenko.homeworks.homework_18;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Data
+@NoArgsConstructor
+public class Geo {
+    String lat;
+    String lng;
+}
